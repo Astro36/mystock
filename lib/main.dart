7aaa93d1b00e089a5b3f52fcc268c85d9867f93e
Seungjae Park <astro.psj@gmail.com>
2023-07-02 21:06:07 +0900

    Add drag-and-drop list template

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import './model.dart';
 import './repository.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'MyStock',
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: const Color(0xBB2649),
+        colorSchemeSeed: const Color(0xFFBB2649),
         fontFamily: 'Pretendard',
       ),
       home: MyHomePage(),
@@ -217,6 +218,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
               },
             ),
           ),
+          Tab(
+            child: IconButton(
+              icon: const Icon(Icons.edit_note),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => MyEditorPage()));
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -230,57 +239,47 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           itemCount: portfolio.stocks.length,
           itemBuilder: (BuildContext context, int index) {
             final stock = portfolio.stocks[index];
-            return Dismissible(
-              key: Key('${portfolio.name}-${stock.ticker}'),
-              child: ListTile(
-                title: Text(stock.ticker),
-                subtitle: Text(stock.name),
-                trailing: FutureBuilder(
-                  future: Future(() async {
-                    if (DateTime.timestamp().difference(stock.priceUpdatedAt).inSeconds > 60) {
-                      stock.price = await YahooFinance.fetchStockPrice(stock.ticker);
-                    }
-                    return stock.price;
-                  }),
-                  builder: (BuildContext context, AsyncSnapshot<StockPrice> snapshot) {
-                    if (snapshot.hasData) {
-                      final priceFormat = NumberFormat.simpleCurrency(name: stock.price.currency);
-                      final priceChangesFormat = NumberFormat('+###.##%;-###.##%;');
-                      return Wrap(
-                        spacing: 8,
-                        children: [
-                          Text(
-                            priceFormat.format(stock.price.value),
-                            style: const TextStyle(color: Colors.black87, fontSize: 14, fontWeight: FontWeight.normal),
+            return ListTile(
+              title: Text(stock.ticker),
+              subtitle: Text(stock.name),
+              trailing: FutureBuilder(
+                future: Future(() async {
+                  if (DateTime.timestamp().difference(stock.priceUpdatedAt).inSeconds > 60) {
+                    stock.price = await YahooFinance.fetchStockPrice(stock.ticker);
+                  }
+                  return stock.price;
+                }),
+                builder: (BuildContext context, AsyncSnapshot<StockPrice> snapshot) {
+                  if (snapshot.hasData) {
+                    final priceFormat = NumberFormat.simpleCurrency(name: stock.price.currency);
+                    final priceChangesFormat = NumberFormat('+###.##%;-###.##%;');
+                    return Wrap(
+                      spacing: 8,
+                      children: [
+                        Text(
+                          priceFormat.format(stock.price.value),
+                          style: const TextStyle(color: Colors.black87, fontSize: 14, fontWeight: FontWeight.normal),
+                        ),
+                        Text(
+                          priceChangesFormat.format(stock.price.changes),
+                          style: TextStyle(
+                            color: stock.price.changes >= 0
+                                ? (stock.price.changes >= 0.05 ? Colors.redAccent[400] : Colors.red[700])
+                                : (stock.price.changes <= -0.05 ? Colors.indigoAccent[400] : Colors.indigo),
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
                           ),
-                          Text(
-                            priceChangesFormat.format(stock.price.changes),
-                            style: TextStyle(
-                              color: stock.price.changes >= 0
-                                  ? (stock.price.changes >= 0.05 ? Colors.redAccent[400] : Colors.red[700])
-                                  : (stock.price.changes <= -0.05 ? Colors.indigoAccent[400] : Colors.indigo),
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ],
-                      );
-                    }
-                    return const CircularProgressIndicator();
-                  },
-                ),
-                onTap: () async {
-                  stock.price = await YahooFinance.fetchStockPrice(stock.ticker);
-                  await _storage.save(_portfolios);
-                  setState(() {});
+                        ),
+                      ],
+                    );
+                  }
+                  return const CircularProgressIndicator();
                 },
               ),
-              onDismissed: (direction) async {
-                portfolio.stocks.removeAt(index);
+              onTap: () async {
+                stock.price = await YahooFinance.fetchStockPrice(stock.ticker);
                 await _storage.save(_portfolios);
-                setState(() {
-                  _focusedTabIndex = _tabController!.index;
-                });
+                setState(() {});
               },
             );
           },
@@ -376,6 +375,68 @@ class _MyCalendarPageState extends State<MyCalendarPage> {
           }
           return const Center(child: CircularProgressIndicator());
         },
+      ),
+    );
+  }
+}
+
+class MyEditorPage extends StatefulWidget {
+  MyEditorPage({super.key});
+
+  @override
+  _MyEditorPageState createState() => _MyEditorPageState();
+}
+
+class _MyEditorPageState extends State<MyEditorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('목록 수정'),
+        centerTitle: true,
+      ),
+      body: DragAndDropLists(
+        children: [
+          DragAndDropList(
+            header: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Header',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            children: [
+              DragAndDropItem(
+                child: ListTile(
+                  title: Text('item'),
+                  subtitle: Text('subtitle'),
+                ),
+              ),
+              DragAndDropItem(
+                child: ListTile(
+                  title: Text('item'),
+                  subtitle: Text('subtitle'),
+                ),
+              )
+            ],
+          )
+        ],
+        axis: Axis.horizontal,
+        listWidth: 150,
+        listDraggingWidth: 150,
+        listDecoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          borderRadius: const BorderRadius.all(Radius.circular(7.0)),
+        ),
+        listPadding: const EdgeInsets.all(8.0),
+        onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) {},
+        onListReorder: (int oldListIndex, int newListIndex) {},
       ),
     );
   }
